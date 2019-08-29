55baa5ad71df595484c5d1163ab392fe728c285e
Ivan Plesskih <terma95@gmail.com>
2019-08-29 09:33:26 +0500

    Added crowns to rating

--- src/views/Rating.vue
+++ src/views/Rating.vue
@@ -3,6 +3,9 @@
     <v-card v-for="(i, ind) in items" :key="i.user" class="mb-2">
       <source-name :id="i.user" :subtitles="[ind + 1 + ' место']">
         <v-btn text color="primary" @click.prevent=""> {{ i.total }} баллов </v-btn>
+        <v-icon v-if="ind === 0" color="#FFD700">mdi-crown</v-icon>
+        <v-icon v-if="ind === 1" color="#C0C0C0">mdi-crown</v-icon>
+        <v-icon v-if="ind === 2" color="#cd7f32">mdi-crown</v-icon>
       </source-name>
     </v-card>
   </v-layout>
