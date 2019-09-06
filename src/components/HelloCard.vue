8f8dadf27d4322dd22cab09989896339daf89d5d
Ivan Plesskih <terma95@gmail.com>
2019-09-06 21:06:28 +0500

    Fixed hello card

--- src/components/HelloCard.vue
+++ src/components/HelloCard.vue
@@ -1,8 +1,15 @@
 <template>
   <v-card>
     <v-card-text class="pb-2 black--text"
-      >Привет! Это - Кудач. Мы собираем живые события и активных людей. Cобытия - ниже, а люди - в
-      <router-link :to="{name: 'rating'}">нашем рейтинге</router-link>
+      ><h3>Привет! Это - Кудач</h3>
+      <br />
+      1. Находи интересные события ВКонтакте и
+      <router-link :to="{name: 'proposals'}">добавляй их к нам</router-link><br />
+      2. Посещай события и присылай нам чекины<br />
+      3. Возглавляй <router-link :to="{name: 'rating'}">наш рейтинг</router-link> и получай
+      призы!<br />
+      <br />
+      Подробности - в <a href="https://vk.com/kuda_ch" target="_blank">нашей группе ВКонтакте</a>
     </v-card-text>
     <v-card-actions class="pt-0">
       <v-btn outlined color="info" href="https://vk.com/kuda_ch" target="_blank">Группа ВК</v-btn>
